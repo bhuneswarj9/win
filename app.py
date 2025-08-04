69614e3ee5ceef00d7203a9d877b00f0b9d690bb
bhuneswarj9 <bhuneswarj9@gmail.com>
2025-08-04 12:28:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,11 @@
-from flask import Flask, request, jsonify
-import sys
-import io
-import traceback
-
+import os
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/run', methods=['POST'])
-def run_code():
-    code = request.json.get('code', '')
-
-    # Capture stdout
-    old_stdout = sys.stdout
-    sys.stdout = mystdout = io.StringIO()
-
-    try:
-        exec(code, {})  # Execute user code
-        output = mystdout.getvalue()
-    except Exception as e:
-        output = traceback.format_exc()
-
-    # Restore stdout
-    sys.stdout = old_stdout
-
-    return jsonify({'output': output})
+@app.route('/')
+def home():
+    return "Hello from Render!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
